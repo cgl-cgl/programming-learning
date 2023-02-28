1cb6be88dc2f122a6e22e4ad47398fc08acef35a
cgl-cgl <pudding.cgl@outlook.com>
2023-02-28 17:56:58 +0800

    20230228

--- sql/2.复杂的查询.sql
+++ sql/2.复杂的查询.sql
@@ -1,6 +1,7 @@
 -- CREATE VIEW <视图名称> (<列名1>,<列名2>,...) AS <SELECT 语句>
 -- drop view <视图名称>
-/*创建出满足下述三个条件的视图（视图名称为 ViewPractice5_1）。使用 product（商品）表作为参照表，假设表中包含初始状态的 8 行数据。
+
+/* 1.创建出满足下述三个条件的视图（视图名称为 ViewPractice5_1）。使用 product（商品）表作为参照表，假设表中包含初始状态的 8 行数据。
 	条件 1：销售单价大于等于 1000 日元。
 	条件 2：登记日期是 2009 年 9 月 20 日。
 	条件 3：包含商品名称、销售单价和登记日期三列。
@@ -13,11 +14,12 @@ from product
 where sale_price >= 1000
 and regist_date = date'20090920';
 
--- 向视图 ViewPractice5_1中插入如下数据，会得到什么样的结果？为什么？
+-- 2.向视图 ViewPractice5_1中插入如下数据，会得到什么样的结果？为什么？
 INSERT into viewpractice5_1 VALUES ('刀子', 300, '2009-11-02');
+-- 视图插入数据时，原表也会插入数据，而原表数据插入时不满足约束条件，所以会报错（因为原表有三个带有NOT NULL约束的字段）
 
 
--- 求出全部商品的平均销售单价sale_price_avg
+-- 3.求出全部商品的平均销售单价sale_price_avg
 -- 标量子查询（查询结果只有一个值）
 select *
 			,(select avg(sale_price) 
