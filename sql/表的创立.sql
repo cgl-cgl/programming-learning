07773f1ed6fda70f84bfdb77afe71490ec49e0d8
cgl-cgl <pudding.cgl@outlook.com>
2023-03-12 17:34:10 +0800

    20230312

--- sql/表的创立.sql
+++ sql/表的创立.sql
@@ -31,6 +31,8 @@ INSERT INTO employee VALUES('1', 'Joe', 70000, '1');
 INSERT INTO employee VALUES('2', 'Henry', 80000, '2');
 INSERT INTO employee VALUES('3', 'Sam', 60000, '2');
 INSERT INTO employee VALUES('4', 'Max', 90000, '1');
+INSERT INTO employee VALUES('5', 'Janet', 69000, '1');
+INSERT INTO employee VALUES('6', 'Randy', 85000, '1');
 
 -- table department
 create table department(
@@ -68,7 +70,7 @@ INSERT INTO score VALUES('2', 93);
 INSERT INTO score VALUES('3', 93);
 INSERT INTO score VALUES('4', 91);
 
--- table_num
+-- table table_num
 create table table_num(
 id VARCHAR(2) not null,
 num int not null,
@@ -91,3 +93,98 @@ insert into table_num values ('13',5);
 insert into table_num values ('14',5);
 insert into table_num values ('15',5);
 insert into table_num values ('16',5);
+
+-- table tree
+CREATE table tree(
+id int not null,
+p_id int,
+primary key(id)
+);
+
+insert into tree values(1,null);
+insert into tree values(2,1);
+insert into tree values(3,1);
+insert into tree values(4,2);
+insert into tree values(5,2);
+
+
+--  employee_manager
+create table employee_manager(
+id char(3) not null,
+name VARCHAR(10) not null,
+department VARCHAR(2) not null,
+managerid char(3),
+PRIMARY KEY(id)
+)
+
+insert into employee_manager values('101', 'John', 'A', null);
+insert into employee_manager values('102', 'Dan', 'A', '101');
+insert into employee_manager values('103', 'James', 'A', '101');
+insert into employee_manager values('104', 'Amy', 'A', '101');
+insert into employee_manager values('105', 'Anne', 'A', '101');
+insert into employee_manager values('106', 'Ron', 'B', '101');
+
+
+-- survey_log
+create table survey_log(
+uid VARCHAR(2) not null,
+action varchar(8) not null,
+question_id VARCHAR(5) not null,
+answer_id varchar(10),
+q_num VARCHAR(10),
+TIMESTAMP int)
+
+insert into survey_log values ('5', 'show', '285', null, '1', 123);
+insert into survey_log values ('5', 'answer', '285', '124124', '1', 124);
+insert into survey_log values ('5', 'show', '369', null, '2', 125);
+insert into survey_log values ('5', 'skip', '369', null, '2', 126);
+
+-- point_2d
+create table point_2d(
+x int not null,
+y int not null
+);
+
+INSERT INTO point_2d VALUES(-1, -1);
+INSERT INTO point_2d VALUES(0, 0);
+INSERT INTO point_2d VALUES(-1, -2);
+
+-- trips
+create table trips(
+id VARCHAR(2) not null,
+client_id VARCHAR(2),
+driver_id VARCHAR(2),
+city_id VARCHAR(2),
+status varchar(20),
+request_at date,
+PRIMARY KEY(id)
+);
+
+insert into trips values('1', '1', '10', '1', 'completed', date'20131001');
+insert into trips values('2', '2', '11', '1', 'cancelled_by_driver', date'20131001');
+insert into trips values('3', '3', '12', '6', 'completed', date'20131001');
+insert into trips values('4', '4', '13', '6', 'cancelled_by_client', date'20131001');
+insert into trips values('5', '1', '10', '1', 'completed', date'20131002');
+insert into trips values('6', '2', '11', '6', 'completed', date'20131002');
+insert into trips values('7', '3', '12', '6', 'completed', date'20131002');
+insert into trips values('8', '2', '12', '12', 'completed', date'20131003');
+insert into trips values('9', '3', '10', '12', 'completed', date'20131003');
+insert into trips values('10', '4', '13', '12', 'cancelled_by_driver', date'20131003');
+
+
+-- users
+create table users(
+users_id VARCHAR(2) not null,
+banned VARCHAR(5),
+role varchar(10),
+PRIMARY KEY(users_id)
+);
+
+insert into users values('1', 'No', 'client');
+insert into users values('2', 'Yes', 'client');
+insert into users values('3', 'No', 'client');
+insert into users values('4', 'No', 'client');
+insert into users values('10', 'No', 'driver');
+insert into users values('11', 'No', 'driver');
+insert into users values('12', 'No', 'driver');
+insert into users values('13', 'No', 'driver');
--- sql/题目.sql
+++ sql/题目.sql
@@ -61,7 +61,115 @@ select id,
 			)a
 where count_num = 4
 
+-- 5.判断树节点的类型
+select a.id,
+			 case when a.p_id is null then 'Root'
+			      when a.p_id is not null and b.mid_id is null then 'Leaf'
+					  else 'Inner' end as Type	
+from tree a
+left join (select DISTINCT p_id as mid_id
+             from tree)b
+on a.id = b.mid_id
 
 
+select a.id,
+			 case when a.p_id is null then 'Root'
+			      when a.p_id is not null and b.mid_id is null then 'Leaf'
+					  else 'Inner' end as Type	
+from tree a
+left join (select DISTINCT p_id as mid_id
+             from tree)b
+on a.id = b.mid_id;
 
+
+-- 6.写一条SQL语句找出有5个下属的主管
+select a.name
+from employee_manager a
+left join(
+					select distinct 
+								 managerid,
+								 count(managerid) over(partition by managerid) as employee_count
+					from employee_manager
+					)b
+on a.id = b.managerid
+where b.employee_count = 5
+
+-- 7.查询回答率最高的问题
+select question_id
+  from(
+			select *,
+						 rank() over(order by answer_rate desc) as answer_rate_order
+			from(
+						select question_id,
+									 sum(case when action='answer' then 1 else 0 end)/sum(case when action='show' then 1 else 0 end) as answer_rate
+						from survey_log
+						group by question_id
+					)t
+				)a
+where answer_rate_order = 1
+
+-- 8.找出每个部门工资前三高的员工
+select a.name,
+       a.employee_name,
+			 a.salary
+ from(
+			select t2.name,
+						 t1.id,
+						 t1.name as employee_name,
+						 t1.salary,
+						 dense_rank() over(partition by t2.name order by t1.salary desc) as department_salary_order
+			from employee t1
+			left join department t2
+			on t1.departmentid = t2.id
+		)a
+where a.department_salary_order <= 3;
+
+
+-- 9.求出这些点中的最短距离并保留2位小数
+select DISTINCT round(dis,2) as distance
+from(
+			select a.x,
+						 a.y,
+						 b.x as x_1,
+						 b.y as y_1,
+						 power((a.x-b.x),2)+power((a.y-b.y),2) as dis,
+						 rank() over (order by (power((a.x-b.x),2)+power((a.y-b.y),2))) as rank_
+						 
+			 from(
+						select x,
+									 y,
+									 row_number() over(order by x,y) as id
+						from point_2d
+					 )a
+			left join(select x,
+											 y,
+											 row_number() over(order by x,y) as id
+									from point_2d
+								)b
+			on a.id != b.id
+)t
+where t.rank_ = 1;
+
+-- 10.查出2013年10月1日至2013年10月3日期间非禁止用户的取消率。基于上表，你的 SQL 语句应返回如下结果，取消率（Cancellation Rate）保留两位小数。
+select request_at,
+       round(sum(case when status != 'completed' then 1 else 0 end)/count(status),2) as completed_rate
+from(
+			select a.*,
+						 b.banned as client_banned,
+						 c.banned as driver_banned
+			from trips a
+			left join users b
+			on a.client_id = b.users_id
+			and b.role = 'client'
+			left join users c
+			on a.driver_id = c.users_id
+			and c.role = 'driver'
+			)t1
+where (t1.client_banned = 'No'
+and t1.driver_banned = 'No')
+and request_at between date'20131001' and date'20131003'
+group by request_at
+order by request_at;
+
+-- s
 
