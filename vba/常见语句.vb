baaac7328c9ac69e63f4c227838e9a7ffd2f42ec
cgl-cgl <pudding.cgl@outlook.com>
2024-03-18 23:57:34 +0800

    vba1

---
+++ vba/常见语句.vb
@@ -0,0 +1,27 @@
+' if条件语句
+sub test1()
+    if 条件 then
+        执行语句
+    elseif
+        执行语句
+    else
+        执行语句
+    end if
+end sub
+
+' for循环
+sub test2()
+
+end sub
+
+' do while……loop循环
+sub test3()
+dim i
+i = 2
+do while 条件
+    如果条件为True执行的代码
+    ……
+    i= i+1
+loop
+
+end sub
